259f208cf5a9c9220ffa0a2573b8147ca4e3b702
Tahseen Zahra <54817285+tahseenzahra@users.noreply.github.com>
2020-10-03 04:45:04 +0500

    code updated to get numbers from user

--- multiply.cpp
+++ multiply.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(void)
 {
-    int num1 = 3;
-    int num2 = 6;
-    int num3 = 9;
-    cout <<"num1 * num2 = "
-         <<num1 * num2
+    int num1, num2, num3;
+
+    cout <<"Enter three integers: ";
+    cin >> num1;
+    cin >> num2;
+    cin >> num3;
+    cout <<"num1 * num2 * num3 = "
+         <<num1 * num2 * num3
          <<endl;
 }
