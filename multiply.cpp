2c1d2b78e20e1398ad5aba27ad4fceab591114d8
Tahseen Zahra <54817285+tahseenzahra@users.noreply.github.com>
2020-10-03 04:12:27 +0500

    added file named multiply

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int num1 = 3;
+    int num2 = 6;
+    int num3 = 9;
+    cout <<"num1 * num2 = "
+         <<num1 * num2
+         <<endl;
+}
