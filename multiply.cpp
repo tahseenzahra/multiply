a194fba16358428e5f5df4e9e4175ab14c5ac5ac
Tahseen Zahra <54817285+tahseenzahra@users.noreply.github.com>
2020-10-03 05:16:10 +0500

    used for loop to get value of numbers

--- multiply.cpp
+++ multiply.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int main(void)
 {
-    int num1, num2, num3;
+    int i, num1, num2, num3;
+    int array[3] = {num1, num2, num3};
 
-    cout <<"Enter three integers: ";
-    cin >> num1;
-    cin >> num2;
-    cin >> num3;
-
+    for(i = 0; i < 3; i++)
+    {
+      cout <<"Enter three integers: ";
+      cin >> array[i];
+    }
 
     cout <<"num1 * num2 * num3 = "
-         <<num1 * num2 * num3
+         <<array[0] * array[1] * array[2]
          <<endl;
 }
